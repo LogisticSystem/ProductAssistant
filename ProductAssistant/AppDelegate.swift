9e7c545809ce017b30324cdb9784b8cf5e2c58de
Илья Халяпин <962426@mail.ru>
2018-04-01 23:25:44 +0300

    Initial Commit

---
+++ ProductAssistant/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  ProductAssistant
+//
+//  Created by Илья Халяпин on 01.04.2018.
+//  Copyright © 2018 Ilya Khalyapin. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    // MARK: - Публичные свойства
+    
+    var window: UIWindow?
+    
+    
+    // MARK: - UIApplicationDelegate
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+}
